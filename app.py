795a84bab70c979304c7dcb804ddb1158879593d
Michael_Alemtsehay <99313463+Michael-Alem@users.noreply.github.com>
2025-01-31 17:18:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -41,10 +41,10 @@ def predict_and_save_image(path_test_car:str, output_image_path:str)-> str:
             for box in result.boxes:
                 x1, y1, x2, y2 = map(int, box.xyxy[0])
                 confidence = box.conf[0]
-                cv2.rectangle(image, (x1, y1), (x2, y2), (0, 0, 255), 2)
+                cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
                 text = f'license-plate {confidence:.1f}%'
                 (w, h), _ = cv2.getTextSize(text, cv2.FONT_HERSHEY_SIMPLEX, 0.3, 1)
-                cv2.rectangle(image, (x1, y1 - h - 5), (x1 + w, y1), (0, 0, 255), -1)
+                cv2.rectangle(image, (x1, y1 - h - 5), (x1 + w, y1), (0, 255, 0), -1)
                 cv2.putText(image, text, (x1, y1 - 5), 
                    cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 0, 0), 1, cv2.LINE_AA)
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
