b91ac517eb33a2fcd1497b65288b1187ca1e2307
Michael_Alemtsehay <99313463+Michael-Alem@users.noreply.github.com>
2025-01-31 17:14:30 +0300

    Update app.py

--- app.py
+++ app.py
@@ -42,8 +42,11 @@ def predict_and_save_image(path_test_car:str, output_image_path:str)-> str:
                 x1, y1, x2, y2 = map(int, box.xyxy[0])
                 confidence = box.conf[0]
                 cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
-                cv2.putText(image, f'{confidence*100:.1f}%', (x1, y1 - 10), 
-                cv2.FONT_HERSHEY_SIMPLEX, 0.25, (0, 0, 0), 1, cv2.LINE_AA)
+                text = f'{confidence*100:.1f}%'
+                (w, h), _ = cv2.getTextSize(text, cv2.FONT_HERSHEY_SIMPLEX, 0.25, 1)
+                cv2.rectangle(image, (x1, y1 - h - 5), (x1 + w, y1), (0, 255, 0), -1)
+                cv2.putText(image, text, (x1, y1 - 5), 
+                   cv2.FONT_HERSHEY_SIMPLEX, 0.25, (0, 0, 0), 1, cv2.LINE_AA)
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
         # Ensure the directory exists before saving
         os.makedirs(os.path.dirname(output_image_path), exist_ok=True)
